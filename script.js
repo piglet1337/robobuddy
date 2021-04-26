ca747e3e5af577c6fcd9bd5bf8261a1d75487ccf
piglet1337 <72782432+piglet1337@users.noreply.github.com>
2021-04-25 21:46:15 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,128 @@
+var speechRecognition = window.webkitSpeechRecognition
+var recognition = new speechRecognition()
+var voices = window.speechSynthesis.getVoices();
+var msg = new SpeechSynthesisUtterance({
+    voice: voices[3],
+    lang: 'en-UK',
+    rate: .9,
+    pitch: 1
+});
+var textbox = $("#textbox")
+var textbox2 = $("#textbox2")
+var instructions = $("#instructions")
+var roboName = "RoboBuddy"
+var message = ''
+
+recognition.continuous = true
+
+recognition.onstart = function () {
+    instructions.text("Voice recognition is on")
+}
+
+recognition.onspeechend = function () {
+    instructions.text("No Activity")
+    recognition.start()
+}
+
+recognition.onerror = function () {
+    instructions.text("Error")
+}
+
+recognition.onresult = function (event) {
+    var current = event.resultIndex;
+    var transcript = event.results[current][0].transcript
+    textbox2.val("")
+
+    message = ''
+    message += transcript
+
+    textbox.val(message)
+
+    message = message.toLowerCase()
+    message = simplify(message)
+    message = message.replace(/\s\s+/g,' ');
+    message = message.trim()
+ 
+    conversation(message)
+}
+
+$("#start-btn").click(function (event) {
+    recognition.start()
+})
+
+function say(result){
+    textbox2.val(result)
+    msg.text = result
+    speechSynthesis.speak(msg);
+}
+
+var map = new Map([
+    ["how are","I am well today, How are you?"],
+    ["great"," "],
+    ["well"," "],
+    ["nice"," "],
+    ["good"," "],
+    ["why sky blue","It is just blue"],
+    ["can have cookie","I am sorry but i have no cookies"],
+    ["what happens if divide by zero","the world explodes"],
+    ["play bee movie","According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."],
+    ["carrot","a carrot is a carrot and a carrot is a carrot"],
+    ["did ever hear tragedy of darth plagueis wise","no"],
+    ["thought not it's not story jedi would tell it's sith legend darth plagueis was dark lord of sith so powerful and so wise he could use force to influence midi-chlorians to create life he had such knowledge of dark side he could even keep ones he cared about from dying","he could actually save people from death"],
+    ["dark side of force pathway to many abilities some consider to be unnatural","what happened to him"],
+    ["he became so powerful that only thing he was afraid of was losing his power which eventually of course he did unfortunately he taught his apprentice everything he knew then his apprentice killed him in his sleep ironic he could save others from death but not himself","is it possible to learn this power"],
+    ["not from jedi","end scene"],
+    ["hi",`Hello I am ${roboName}`]
+])
+
+function conversation(message){
+    if (message.startsWith("can change your name to ")){
+        roboName = message.substring(24)
+        say(`Ok I am now ${roboName}`)
+        map.set("hi",`Hello I am ${roboName}`)
+        return
+    }
+    testForMessage(message)
+}
+
+
+function testForMessage(string){
+    let stringSplit = string.split(' ')
+    if (map.get(string)) {
+        say(map.get(string))
+        return
+    }
+    for (let i = 0; i < stringSplit.length; i++){
+        let stringWordRemoved = remove(string, stringSplit, i)
+        if(map.get(stringWordRemoved)) {
+            say(map.get(stringWordRemoved))
+            return
+        }
+        let stringSplit2 = stringWordRemoved.split(' ')
+        for (let j = 0; j < stringSplit2.length; j++){
+            let stringTwoRemoved = remove(stringWordRemoved, stringSplit2, j)
+            if (map.get(stringTwoRemoved)) {
+                say(map.get(stringTwoRemoved))
+                return
+            }
+        }
+    }
+    say("I don't understand")
+}
+
+function remove(string, stringSplit, i){
+    let newString = string.replace(stringSplit[i],'')
+    newString = newString.trim()
+    newString = newString.replace(/\s\s+/g, ' ') 
+    return newString
+}
+
+
+function simplify(message){
+    let mapObj = {hello:"hi",hey:"hi",greetings:"hi",is:'',the:'',i:'',a:'',you:''};
+    let re = new RegExp("\\b(?:" + Object.keys(mapObj).join("|") + ")\\b","gi");
+    message = message.replace(re, function(matched){
+        return mapObj[matched];
+    });
+    return message
+}
