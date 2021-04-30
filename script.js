11ea9f9867283822a5a762b3877c52b83d12866c
piglet1337 <72782432+piglet1337@users.noreply.github.com>
2021-04-30 12:57:35 -0700

    cleanly code

--- script.js
+++ script.js
@@ -1,55 +1,43 @@
-let speechRecognition = window.webkitSpeechRecognition
-let recognition = new speechRecognition()
-let msg = new SpeechSynthesisUtterance()
-let voices = window.speechSynthesis.getVoices()
-let textbox = $("#textbox")
-let textbox2 = $("#textbox2")
-let instructions = $("#instructions")
+const speechRecognition = window.webkitSpeechRecognition
+const recognition = new speechRecognition()
+const textbox = $("#textbox")
+const textbox2 = $("#textbox2")
+const instructions = $("#instructions")
 let roboName = "RoboBuddy"
-recognition.continuous = true
 
-recognition.onstart = function () {
-    instructions.text("Voice recognition is on")
-}
+recognition.continuous = true
+window.speechSynthesis.getVoices()
+
+recognition.onstart = () => instructions.text("Voice recognition is on")
 
 recognition.addEventListener('end', recognition.start)
 
-recognition.onerror = function () {
-    instructions.text("Error")
-}
+recognition.onerror = () => instructions.text("Error")
 
-recognition.onresult = function (event) {
+recognition.onresult = (event) => {
     let current = event.resultIndex
     let message = event.results[current][0].transcript
     textbox2.val("")
     textbox.val(message)
     message = simplify(message)
-    checkAllInputs(message)
+    checkAllInputs(message, map)
 }
 
-$("#start-btn").click(function (event) {
-    recognition.start()
-})
+$("#start-btn").click( () => recognition.start())
 
 function simplify(message){
     let mapObj = {hello:"hi",hey:"hi",greetings:"hi",is:'',i:'',the:'',a:'',of:'',are:''}
     let re = new RegExp("\\b(?:" + Object.keys(mapObj).join("|") + ")\\b","gi")
-    message = message.replace(re, function(matched){
-        return mapObj[matched.toLowerCase()]
-    })
+    message = message.replace(re, (matched) => mapObj[matched.toLowerCase()])
     message = message.replace(/\s\s+/g,' ').trim().toLowerCase()
-    textbox.val(message)
     return message
 }
 
 function say(result, string){
     textbox2.val(result)
-    let voices = window.speechSynthesis.getVoices()
+    const voices = window.speechSynthesis.getVoices()
+    const msg = new SpeechSynthesisUtterance(result)
     //msg.voice = voices[3]
-    msg.lang = 'en-UK'
-    msg.rate = .9
-    msg.pitch = 1
-    msg.text = result
     if (result === "NAMECHANGE"){
         roboName = string.split(" ").pop()
         roboName = roboName.charAt(0).toUpperCase() + roboName.slice(1)
@@ -61,12 +49,10 @@ function say(result, string){
     speechSynthesis.speak(msg)
 }
 
-function checkAllInputs(string){
-    let keys = Array.from(map.keys()).sort(function(a, b){
-        return b.split(' ').length - a.split(' ').length
-    })
+function checkAllInputs(string, map){
+    const keys = Array.from(map.keys()).sort( (a, b) => b.split(' ').length - a.split(' ').length)
     for (let i = 0; i < keys.length; i++){
-        let re = new RegExp("(?=.* " + keys[i].replace(' '," )(?=.* ") + " ).*")
+        const re = new RegExp("(?=.* " + keys[i].replace(' '," )(?=.* ") + " ).*")
         if (re.test(` ${string} `)){
             say(map.get(keys[i]),string)
             return
@@ -75,7 +61,7 @@ function checkAllInputs(string){
     say("I'm sorry but I am having trouble understanding please rephrase")
 }
 
-let map = new Map([
+const map = new Map([
     ["how you","I am well today, How are you?"],
     ["great"," "],
     ["well"," "],
